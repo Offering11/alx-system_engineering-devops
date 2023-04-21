67b895c8e6fcf1c7d2b803541601df615edb8a45
Offering11 <lawaloffering@gmail.com>
2023-04-21 12:35:04 -0700

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { '/tmp/codingschool':
-  ensure  => file,
-  path    => '/tmp/codingschool',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+# Using Puppet
+
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
